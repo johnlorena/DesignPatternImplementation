b1bd92fe0e8b05fd548d77fc88b4381236dac00b
JohnLorena <mlorena@you-source.com>
2024-05-08 11:44:13 +0800

    Added liskov- New component working

--- SOLIDpatterns/src/app/app.routes.ts
+++ SOLIDpatterns/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LiskovComponent } from './liskov/liskov.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'liskov', component: LiskovComponent }
+];
---
+++ SOLIDpatterns/src/app/liskov/liskov.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liskov',
+  standalone: true,
+  imports: [],
+  templateUrl: './liskov.component.html',
+  styleUrl: './liskov.component.css'
+})
+export class LiskovComponent {
+
+}
